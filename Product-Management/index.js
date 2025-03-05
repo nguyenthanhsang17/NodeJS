a3cfd86786ebb5fc67e781979ea3ef897686bc48
nguyenthanhsang17 <nguyenthanhsang17102002@gmail.com>
2025-03-05 15:18:29 +0700

    Sang commit

--- Product-Management/index.js
+++ Product-Management/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const port = process.env.PORT;
 
 const route = require('./routes/client/index.route.js');
-
+//sang
 app.set('views', './views');
 app.set('view engine', 'pug');
 
